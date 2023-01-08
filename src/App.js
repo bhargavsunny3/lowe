955bb783e4e1dd02b662c228599b42685eb3fa6d
bhargavsunny3 <sbhargav788@gmail.com>
2023-01-08 19:46:55 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Sarala
+          Learn React with Chandishwars
         </a>
       </header>
     </div>
